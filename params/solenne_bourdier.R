b4d5046ff95f4da0b954db4155018c69137c9cdb
collazoa <anja.collazo@charite.de>
2024-01-26 09:18:53 +0100

    solenne

--- params/solenne_bourdier.R
+++ params/solenne_bourdier.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 2.3
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "solenne_test"
     
 # e.g. "red"    
-color <- 
+color <- "red"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
